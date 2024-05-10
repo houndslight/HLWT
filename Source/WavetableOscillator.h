cd651912f87f7790ee7cb03ca81d6d85d5edfe60
zachary <ocean@outlook.lv>
2024-05-10 19:30:16 -0400

    Add files via upload

---
+++ Source/WavetableOscillator.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+
+class WavetableOscillator
+{
+public:
+	WavetableOscillator(std::vector<float> waveTable, double sampleRate);
+
+	void setFrequency(float frequency);
+	float getSample();
+
+	void stop();
+	bool isPlaying();
+
+private:
+
+	float interpolateLineraly();
+
+	std::vector<float> waveTable;
+	double sampleRate;
+	float index = 0.f;
+	float indexIncrement = 0.f;
+
+};
+
